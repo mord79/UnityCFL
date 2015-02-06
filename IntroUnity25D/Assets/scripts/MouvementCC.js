1218cad1160495796c28beabfb5cd14f295730df
mord79 <mord79@gmail.com>
2015-02-06 16:56:26 -0500

    Début du projet d'initiation sec2, script mouvement
    gossage avec le characterController

---
+++ IntroUnity25D/Assets/scripts/MouvementCC.js
@@ -0,0 +1,62 @@
+﻿#pragma strict
+
+var mouvementSpeed : float;
+var jumpSpeed : float;
+var gravity : float;
+var gravitySpeed : float;
+
+function Start () {
+
+	
+
+}
+
+function Update () {
+	
+	
+	var controller : CharacterController = this.GetComponent(CharacterController);
+	if(Input.GetKey(KeyCode.A)){
+			controller.Move(Vector3(mouvementSpeed*Time.deltaTime,0,0));
+	}else if(Input.GetKey(KeyCode.D)){
+			controller.Move(Vector3(-mouvementSpeed*Time.deltaTime,0,0));
+	}else if(Input.GetKey(KeyCode.W)){
+			controller.Move(Vector3(0,0,mouvementSpeed*Time.deltaTime));
+	}else if(Input.GetKey(KeyCode.S)){
+			controller.Move(Vector3(0,0,-mouvementSpeed*Time.deltaTime));
+	}
+	
+ 
+	if(controller.isGrounded ){
+		gravitySpeed = 0;
+		if(Input.GetKey(KeyCode.Space)){
+			Jump(jumpSpeed);
+			}
+	}else if(!controller.isGrounded){
+			controller.Move(Vector3.down*gravitySpeed*Time.deltaTime);
+			if(gravitySpeed < gravity){
+			gravitySpeed ++;
+			}
+	}
+	
+}
+
+
+
+function Jump(strengt : float){
+	var controller : CharacterController = this.GetComponent(CharacterController);
+	var jstrengt = strengt;
+		
+	while(jstrengt > 0){
+		controller.Move(Vector3(0,jstrengt*Time.deltaTime,0));
+		jstrengt --;
+		yield WaitForEndOfFrame;
+		
+	
+	}
+
+
+
+}
+
+
+@script RequireComponent(CharacterController);
