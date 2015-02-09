019912bbe0fb96c3c299bd02c8fde34590c12bae
mord79 <mord79@gmail.com>
2015-02-09 16:59:10 -0500

    Update Script
    UI script

---
+++ IntroUnity25D/Assets/scripts/EndLevel.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var nbOfCollectible : GameObject[];
+
+
+function Start () {
+
+
+
+}
+
+function Update () {
+nbOfCollectible = GameObject.FindGameObjectsWithTag("Collectible");
+	
+	
+	if(nbOfCollectible.length == 0){
+	Debug.Log("fin");
+	Application.LoadLevel("scene2");
+	}
+}
--- IntroUnity25D/Assets/scripts/Score.js
+++ IntroUnity25D/Assets/scripts/Score.js
@@ -1,10 +1,22 @@
 ﻿#pragma strict
 
+import UnityEngine.UI;
+
+var text : Text;
+text = GameObject.Find("Score").GetComponent(Text);
 
 var score : int;
 
-function AddScore(number : int){
+function Start () {
 
+	text.text = "Score: " + score;
+
+
+}
+
+function AddScore(number : int){
+	
 	score += number;
+	text.text = "Score: " + score;
 
 }
