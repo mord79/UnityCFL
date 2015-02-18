0ba735ae52005a6555f6744c03dbab01318a08c7
mord79 <mord79@gmail.com>
2015-02-18 13:02:07 -0500

    Sons 3D

---
+++ ProjetSon3D/Son3D/Assets/Unity4_Example_NonComm/Assets/Scripts/Editor/AssetImporter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class AssetImporter : EditorWindow {
+	
+	public static AssetImporter instance { get; private set; }
+	
+	[MenuItem ("3Dception/Import")]
+	static void Init () {
+		// Get existing open window or if none, make a new one:		
+		instance = (AssetImporter) EditorWindow.GetWindow (typeof(AssetImporter));
+	}
+	
+	void OnGUI ()
+	{	
+		if (GUILayout.Button ("Import")) 
+		{
+			import ();
+		}
+	}
+	
+	static void import () 
+	{
+		string[] args = System.Environment.GetCommandLineArgs ();
+		string TYPE = args [7];
+		string VERSION = args [8];
+		string UNITY = args [9];
+		AssetDatabase.ImportPackage("../../3Dception_" + VERSION + "_Win_" + TYPE + "_" + UNITY + ".unitypackage", false);
+		AssetDatabase.ImportPackage("../../3Dception_" + VERSION + "_OSX_" + TYPE + "_" + UNITY + ".unitypackage", false);
+	}
+	
+}
---
+++ ProjetSon3D/Son3D/Assets/Unity4_Example_NonComm/README.txt
@@ -0,0 +1,8 @@
+README
+------
+
+This is an example project for Unity3D for 3Dception. 
+
+1) Load: 3Dception_Example.unity
+2) Read the included documentation on how to setup 3Dception
+3) Play the scene!
