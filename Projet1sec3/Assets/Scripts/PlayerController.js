0898adf86e956fb4714ea9ba36517f0ec823b191
mord79 <mord79@gmail.com>
2015-03-12 10:19:26 -0400

    stuff

--- Projet1sec3/Assets/Scripts/PlayerController.js
+++ Projet1sec3/Assets/Scripts/PlayerController.js
@@ -1,41 +1,49 @@
 ﻿#pragma strict
+
+// vitesse maximum de mon objet
 var maxSpeed : float;
 
+// magnitude est la vitesse absolue de mon objet
 var magnitude : float;
 
 var speed : float;
 var jump : float;
 var monNom : String; 
+// Variable pour aller chercher mon RigidBody
+private var myRigidBody : Rigidbody;
 
 
 function Start () {
 
+	// Affecter ma variable myRigidBody
+	myRigidBody = GetComponent(Rigidbody);	
+	
 	Debug.Log("Bonjour je suis " + monNom);
 
 }
 
 function Update () {
-	magnitude = this.rigidbody.velocity.magnitude; 
+	magnitude = myRigidBody.velocity.magnitude; 
 	
 
 		if(Input.GetKey(KeyCode.RightArrow) && magnitude < maxSpeed){
-			rigidbody.AddForce(Vector3.right*speed);
+			myRigidBody.AddForce(Vector3.right*speed);
 		
 		} else if (Input.GetKey(KeyCode.LeftArrow) && magnitude < maxSpeed){
-			rigidbody.AddForce(Vector3.left*speed);
+			myRigidBody.AddForce(Vector3.left*speed);
 		
 		}
 		
 		if (Input.GetKey(KeyCode.UpArrow) && magnitude < maxSpeed){
-			rigidbody.AddForce(Vector3.forward*speed);
+			myRigidBody.AddForce(Vector3.forward*speed);
 		
 		} else if (Input.GetKey(KeyCode.DownArrow) && magnitude < maxSpeed){
-			rigidbody.AddForce(Vector3.back*speed);
+			myRigidBody.AddForce(Vector3.back*speed);
 		
 		}
 		
 		if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down),1)){
-			rigidbody.AddForce(Vector3.up*jump);
+			myRigidBody.AddForce(Vector3.up*jump);
 			
 		}
 		
